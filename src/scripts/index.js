5eefab050d0ac7380794109a4c1b7ece9514d0e5
Diandara TResya Haniva <diandaraa@gmail.com>
2022-04-21 03:12:57 +0700

    navbar & hero element (jumbotron)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,16 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+const hamburgerButtonElement = document.querySelector("#dropdown");
+const drawerElement = document.querySelector("#nav-list");
+const mainElement = document.querySelector("main");
+
+hamburgerButtonElement.addEventListener("click", event => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+});
+
+mainElement.addEventListener("click", event => {
+    drawerElement.classList.remove("open");
+    event.stopPropagation();
+})
